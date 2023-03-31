4a5ab59b08e07dbec475f3fd78bf55a99e00bfab
Stephany Henrique Batista <stephany.batista@outlook.com>
2023-03-30 20:25:30 -0400

    created delete and update endpoints

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -27,6 +27,8 @@ func main() {
 	}
 	r.Post("/campaigns", endpoints.HandlerError(handler.CampaignPost))
 	r.Get("/campaigns/{id}", endpoints.HandlerError(handler.CampaignGetById))
+	r.Patch("/campaigns/cancel/{id}", endpoints.HandlerError(handler.CampaignCancelPatch))
+	r.Delete("/campaigns/delete/{id}", endpoints.HandlerError(handler.CampaignDelete))
 
 	http.ListenAndServe(":3000", r)
 }
