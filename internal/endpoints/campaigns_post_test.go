c22726fcae2e40c943df4e07a0298da84a96ab4c
Stephany Henrique Batista <stephany.batista@outlook.com>
2023-06-09 15:18:40 -0400

    Tested the campaign_post

--- internal/endpoints/campaigns_post_test.go
+++ internal/endpoints/campaigns_post_test.go
@@ -2,6 +2,7 @@ package endpoints
 
 import (
 	"bytes"
+	"context"
 	"emailn/internal/contract"
 	internalmock "emailn/internal/test/internal-mock"
 	"encoding/json"
@@ -14,8 +15,20 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
+func setup(body contract.NewCampaign, createdByExpected string) (*http.Request, *httptest.ResponseRecorder) {
+	var buf bytes.Buffer
+	json.NewEncoder(&buf).Encode(body)
+	req, _ := http.NewRequest("POST", "/", &buf)
+	ctx := context.WithValue(req.Context(), "email", createdByExpected)
+	req = req.WithContext(ctx)
+	rr := httptest.NewRecorder()
+
+	return req, rr
+}
+
 func Test_CampaignsPost_should_save_new_camapaign(t *testing.T) {
 	assert := assert.New(t)
+	createdByExpected := "teste1@teste.com.br"
 	body := contract.NewCampaign{
 		Name:    "teste",
 		Content: "Hi everyone",
@@ -23,18 +36,16 @@ func Test_CampaignsPost_should_save_new_camapaign(t *testing.T) {
 	}
 	service := new(internalmock.CampaignServiceMock)
 	service.On("Create", mock.MatchedBy(func(request contract.NewCampaign) bool {
-		if request.Name == body.Name && request.Content == body.Content {
+		if request.Name == body.Name &&
+			request.Content == body.Content &&
+			request.CreatedBy == createdByExpected {
 			return true
 		} else {
 			return false
 		}
 	})).Return("34x", nil)
 	handler := Handler{CampaignService: service}
-
-	var buf bytes.Buffer
-	json.NewEncoder(&buf).Encode(body)
-	req, _ := http.NewRequest("POST", "/", &buf)
-	rr := httptest.NewRecorder()
+	req, rr := setup(body, createdByExpected)
 
 	_, status, err := handler.CampaignPost(rr, req)
 
@@ -52,11 +63,7 @@ func Test_CampaignsPost_should_inform_error_when_exist(t *testing.T) {
 	service := new(internalmock.CampaignServiceMock)
 	service.On("Create", mock.Anything).Return("", fmt.Errorf("error"))
 	handler := Handler{CampaignService: service}
-
-	var buf bytes.Buffer
-	json.NewEncoder(&buf).Encode(body)
-	req, _ := http.NewRequest("POST", "/", &buf)
-	rr := httptest.NewRecorder()
+	req, rr := setup(body, "teste@teste.com.br")
 
 	_, _, err := handler.CampaignPost(rr, req)
 
